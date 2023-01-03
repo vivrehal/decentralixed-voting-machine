515aa9518df507136b835383e065ccd080d2e1f2
vivrehal <dimanvivek777@gmail.com>
2023-01-03 13:33:24 +0530

    testing

--- dvm/pages/voterList.js
+++ dvm/pages/voterList.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from "react";
 
+import VoterCard from "../components/voterCard/voterCard";
+import Style from "../styles/voterList.module.css";
+
+import { VotingContext } from "../context/Voter";
 const voterList = () => {
-    
-        return (
-            <div>
-                voterList
-            </div>
-        );
-}
+  const { getAllVoterData, voterArray } = useContext(VotingContext);
+
+  useEffect(() => {
+    getAllVoterData();
+    console.log(voterArray);
+  }, []);
+
+  return (
+    <div className={Style.voterList}>
+      <VoterCard voterArray={voterArray} />
+    </div>
+  );
+};
 
 export default voterList;
